530c6946293e1aabd2e108dc9dd3d9a800ad221d
Bella Hill <ihi17@uclive.ac.nz>
2026-05-26 08:22:39 +1200

    improving validation for editing

--- src/app/overview/overview.ts
+++ src/app/overview/overview.ts
@@ -61,52 +61,13 @@ export class Overview implements OnInit{
   }
 
   async onSubmit() {
+    this.editingBookmark = null;
     this.clearErrors();
     this.bookmarkForm.updateValueAndValidity();
     const urlInput = this.bookmarkForm.get('link');
+
+    if (await this.validateUrl(this.bookmarkForm, urlInput, 'link')) return;
     
-    // First check if URL has been entered, 
-    // then check if it is a duplicate, 
-    // then check if it is valid format.
-    // Add relevant errors for each case.
-    if (this.bookmarkForm.invalid) {
-      if (urlInput?.hasError('required')) {
-        this.bookmarkService.addUrlError('URL must not be empty.');
-      } 
-      else if (urlInput?.hasError('duplicateUrl')) {
-        this.bookmarkService.addUrlError('URL already bookmarked.');
-        this.highlightDuplicateBookmark(urlInput);
-      } 
-      else if (urlInput?.hasError('invalidUrlFormat')) {
-        this.bookmarkService.addUrlError('Not a valid URL.');
-      }
-      this.urlError = this.bookmarkService.getError();
-      this.toggleErrors('link');
-      return;
-    }
-
-    // This is to make sure the URL checker has time to process.
-    const finished = race(
-      this.bookmarkForm.statusChanges.pipe(
-        filter(status => status !== 'PENDING'),
-        take(1)
-      ),
-      timer(5000) 
-    );
-
-    await firstValueFrom(finished);
-
-    // Check that URL is a real URL.
-    if (this.bookmarkForm.invalid) {
-      if (urlInput?.hasError('invalidUrl')) {
-        this.bookmarkService.addUrlError('URL not found.');
-
-      }
-      this.urlError = this.bookmarkService.getError();
-      this.toggleErrors('link');
-      return;
-    }
-
     // Add normalised URL to local storage and take user to results page.
     const normalisedUrl = this.bookmarkService.normaliseUrl(String(urlInput?.value));
     this.bookmarkService.addBookmark(normalisedUrl);  
@@ -124,30 +85,39 @@ export class Overview implements OnInit{
       this.updateErrorsAndBookmarks();
       return;
     }
+
+    if (await this.validateUrl(this.editForm, urlInput, 'edit')) return;
     
+    const normalisedUrl = this.bookmarkService.normaliseUrl(String(urlInput?.value));
+    this.bookmarkService.editBookmark(oldUrl!, normalisedUrl);  
+    this.editingBookmark = null;
+    this.updateErrorsAndBookmarks();
+  }
+
     // First check if URL has been entered, 
     // then check if it is a duplicate, 
     // then check if it is valid format.
     // Add relevant errors for each case.
-    if (this.editForm.invalid) {
+  async validateUrl(form: any, urlInput: any, field: string): Promise<boolean> {
+    if (form.invalid) {
       if (urlInput?.hasError('required')) {
         this.bookmarkService.addUrlError('URL must not be empty.');
       } 
       else if (urlInput?.hasError('duplicateUrl')) {
         this.bookmarkService.addUrlError('URL already bookmarked.');
-        this.highlightDuplicateBookmark(urlInput);
+        if (field === 'link') this.highlightDuplicateBookmark(urlInput);
       } 
       else if (urlInput?.hasError('invalidUrlFormat')) {
         this.bookmarkService.addUrlError('Not a valid URL.');
       }
       this.urlError = this.bookmarkService.getError();
-      this.toggleErrors('edit');
-      return;
+      this.toggleErrors(field);
+      return true;
     }
 
     // This is to make sure the URL checker has time to process.
     const finished = race(
-      this.editForm.statusChanges.pipe(
+      form.statusChanges.pipe(
         filter(status => status !== 'PENDING'),
         take(1)
       ),
@@ -157,21 +127,16 @@ export class Overview implements OnInit{
     await firstValueFrom(finished);
 
     // Check that URL is a real URL.
-    if (this.editForm.invalid) {
+    if (form.invalid) {
       if (urlInput?.hasError('invalidUrl')) {
         this.bookmarkService.addUrlError('URL not found.');
 
       }
       this.urlError = this.bookmarkService.getError();
-      this.toggleErrors('edit');
-      return;
+      this.toggleErrors(field);
+      return true;
     }
-
-    // Add normalised URL to local storage and take user to results page.
-    const normalisedUrl = this.bookmarkService.normaliseUrl(String(urlInput?.value));
-    this.bookmarkService.editBookmark(oldUrl!, normalisedUrl);  
-    this.editingBookmark = null;
-    this.updateErrorsAndBookmarks();
+    return false;
   }
 
   /**
@@ -196,8 +161,14 @@ export class Overview implements OnInit{
   }
 
   editBookmark(bookmarkToEdit: string): void {
+    this.clearErrors();
     this.editingBookmark = bookmarkToEdit;
     this.editForm.get('edit')?.setValue(this.editingBookmark);
+  }
+
+  closeEditing() {
+  this.editingBookmark = null;
+  this.clearErrors();
   }
 
   clearErrors(): void {
@@ -205,12 +176,17 @@ export class Overview implements OnInit{
     this.urlError = this.bookmarkService.getError();
     this.duplicateBookmark = null;
     this.toggleErrors('link');
+    this.toggleErrors('edit');
   }
 
   toggleErrors(field: string): void {
     const input = document.getElementById(field) as HTMLElement;
     const hasErrors = !!this.bookmarkService.getError();
-    input.classList.toggle('input-error', hasErrors);
+    if (field === 'link') {
+      input?.classList.toggle('input-error', hasErrors);
+    } else if (field === 'edit') {
+      input?.classList.toggle('edit-error', hasErrors); 
+    }
   }
 
   updateErrorsAndBookmarks(): void {
@@ -225,9 +201,6 @@ export class Overview implements OnInit{
    * @param urlInput URL already bookmarked by user.
    */
   highlightDuplicateBookmark(urlInput: any): void {
-    // Clear input from duplicated bookmark attempt.
-    const input = document.getElementById('link') as HTMLInputElement;
-    input.value = '';
     this.duplicateBookmark = this.bookmarkService.normaliseUrl(String(urlInput?.value));
     // Get index of duplicated bookmark.
     const index = this.allBookmarks.indexOf(this.duplicateBookmark);
